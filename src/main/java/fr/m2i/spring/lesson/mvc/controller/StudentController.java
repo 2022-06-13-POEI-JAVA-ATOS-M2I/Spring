a5ffe4188cb68f565cdbdc049a8243ff6fe9bb63
Filip Iricanin <irican_f@etna-alternance.net>
2022-07-31 19:40:05 +0200

    Spring boot mvc form

---
+++ src/main/java/fr/m2i/spring/lesson/mvc/controller/StudentController.java
@@ -0,0 +1,29 @@
+package fr.m2i.spring.lesson.mvc.controller;
+
+import fr.m2i.spring.lesson.mvc.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StudentController {
+
+    @RequestMapping(value = "/student", method = RequestMethod.GET)
+    public ModelAndView student() {
+        return new ModelAndView("student", "command", new Student());
+    }
+
+    @RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+    public String addStudent(@ModelAttribute Student student, ModelMap model) {
+        model.addAttribute("name", student.getName());
+        model.addAttribute("age", student.getAge());
+        model.addAttribute("id", student.getId());
+
+        return "result";
+    }
+}
+
+
---
+++ src/main/java/fr/m2i/spring/lesson/mvc/model/Student.java
@@ -0,0 +1,27 @@
+package fr.m2i.spring.lesson.mvc.model;
+
+
+public class Student {
+   private Integer age;
+   private String name;
+   private Integer id;
+
+   public void setAge(Integer age) {
+      this.age = age;
+   }
+   public Integer getAge() {
+      return age;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getName() {
+      return name;
+   }
+   public void setId(Integer id) {
+      this.id = id;
+   }
+   public Integer getId() {
+      return id;
+   }
+}
