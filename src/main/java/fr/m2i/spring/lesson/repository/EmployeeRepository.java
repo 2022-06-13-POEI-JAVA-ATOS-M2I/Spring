53199ee71a45f929b363f6053b1f7cf9172d4342
Filip Iricanin <irican_f@etna-alternance.net>
2022-07-31 15:29:09 +0200

    Spring boot mvc

--- src/main/java/fr/m2i/spring/lesson/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.m2i.spring.lesson.repository;
-
-import fr.m2i.spring.lesson.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
